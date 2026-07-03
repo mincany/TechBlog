e6e20c6691c91d4a78732e3fd8de815bfa4c409f
Mincan Yang <mincany@amazon.com>
2026-07-02 19:47:21 -0700

    fix: remove deleted projects collection reference from ArrowCard

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@ import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
   NAME: "Mincan Yang",
-  EMAIL: "mincan.yang@outlook.com",
+  EMAIL: "mincany0708@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 3,
   NUM_PROJECTS_ON_HOMEPAGE: 0,
